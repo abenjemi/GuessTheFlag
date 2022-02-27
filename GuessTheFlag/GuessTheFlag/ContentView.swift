c94245ba4b2a429c5fc1544543e572fa42054aea
Amine Ben Jemia <aminebenjemia8@gmail.com>
2022-02-27 16:55:25 -0500

    changed fonts and added some styling

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,56 +10,77 @@ import SwiftUI
 struct ContentView: View {
     
     @State var countries = ["estonia", "france", "germany", "ireland"]
-    var countryChosen = Int.random(in: 0...2)
+    @State var countryChosen = Int.random(in: 0...2)
     @State var myScore = 0
     @State var showAlert = false
-    @State var answer = false
+    @State var answer = ""
         
     var body: some View {
         
         ZStack {
             
-            Color.blue
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .top, endPoint: .bottom)
             
             VStack(spacing: 30) {
+                
+                Spacer()
+                
                 VStack {
                     Text("Tap the flag of")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        
                     Text("\(countries[countryChosen])")
+                        .font(.largeTitle.bold())
+                        .textCase(.uppercase)
+                        .padding(.top, 5.0)
                 }
                 .foregroundColor(.white)
+                .padding()
 
                 ForEach(0..<3) { number in
                     Button {
                         answer = isCorrect(countries: countries, randNumber: countryChosen, number: number, &myScore)
                         showAlert = true
                         countries.shuffle()
+                        countryChosen = Int.random(in: 0...2)
                     } label: {
                         Image(countries[number])
+                            .renderingMode(.original)
                     }
-                    .alert("Score", isPresented: $showAlert) {
+                    .alert(answer, isPresented: $showAlert) {
                         Button("OK") {}
                     } message: {
-                        if answer {
-                            Text("Correct answer: Your score is \(myScore)")
-                        }
-                        else {
-                            Text("Wrong answer: Your score is \(myScore)")
-                        }
-                        
+                        Text("Your score is \(myScore)")
                     }
                     
                 }
+                
+                Spacer()
+                
+                Button("Restart") {
+                    myScore = 0
+                }
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 150, height: 50)
+                .background(Color.black)
+                .cornerRadius(15.0)
+                
+                Spacer()
             }
         }
+        .ignoresSafeArea()
     }
 }
 
-func isCorrect(countries:[String], randNumber:Int, number:Int, _ score: inout Int) -> Bool {
+func isCorrect(countries:[String], randNumber:Int, number:Int, _ score: inout Int) -> String {
     if countries[randNumber] == countries[number] {
         score += 1
-        return true
+        return "Correct Answer"
     }
-    return false
+    return "Wrong Answer"
 }
 
 struct ContentView_Previews: PreviewProvider {
