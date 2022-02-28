bacd6fceba1baf7bacd534e38acd168ae33f2a04
Amine Ben Jemia <aminebenjemia8@gmail.com>
2022-02-27 19:44:39 -0500

    added score at the top right corner

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var countries = ["estonia", "france", "germany", "ireland"]
+    @State var countries = ["estonia", "france", "germany", "ireland"].shuffled()
     @State var countryChosen = Int.random(in: 0...2)
     @State var myScore = 0
     @State var showAlert = false
@@ -20,58 +20,69 @@ struct ContentView: View {
         ZStack {
             
             LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            ZStack (alignment: .topTrailing) {
+                
+                Text("Score: \(myScore)")
+                    .font(.subheadline.weight(.black))
+                    .foregroundColor(.white)
+                    .offset(x: 60)
+                    
+                
             
-            VStack(spacing: 30) {
-                
-                Spacer()
-                
-                VStack {
-                    Text("Tap the flag of")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        
-                    Text("\(countries[countryChosen])")
-                        .font(.largeTitle.bold())
-                        .textCase(.uppercase)
-                        .padding(.top, 5.0)
-                }
-                .foregroundColor(.white)
-                .padding()
-
-                ForEach(0..<3) { number in
-                    Button {
-                        answer = isCorrect(countries: countries, randNumber: countryChosen, number: number, &myScore)
-                        showAlert = true
-                        countries.shuffle()
-                        countryChosen = Int.random(in: 0...2)
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
+                VStack(spacing: 30) {
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            
+                        Text("\(countries[countryChosen])")
+                            .font(.largeTitle.bold())
+                            .textCase(.uppercase)
+                            .padding(.top, 5.0)
                     }
-                    .alert(answer, isPresented: $showAlert) {
-                        Button("OK") {}
-                    } message: {
-                        Text("Your score is \(myScore)")
+                    .foregroundColor(.white)
+                    .padding()
+
+                    ForEach(0..<3) { number in
+                        Button {
+                            answer = isCorrect(countries: countries, randNumber: countryChosen, number: number, &myScore)
+                            showAlert = true
+                            countries.shuffle()
+                            countryChosen = Int.random(in: 0...2)
+                        } label: {
+                            Image(countries[number])
+                                .resizable()
+                                .scaledToFit()
+                            
+                        }
+                        .alert(answer, isPresented: $showAlert) {
+                            Button("OK") {}
+                        } message: {
+                            Text("Your score is \(myScore)")
+                        }
+                        
                     }
                     
+                    Spacer()
+                    
+                    Button("Restart") {
+                        myScore = 0
+                    }
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 150, height: 50)
+                    .background(Color.black)
+                    .cornerRadius(15.0)
+                    
+                    Spacer()
                 }
-                
-                Spacer()
-                
-                Button("Restart") {
-                    myScore = 0
-                }
-                .font(.largeTitle)
-                .foregroundColor(.white)
-                .padding()
-                .frame(width: 150, height: 50)
-                .background(Color.black)
-                .cornerRadius(15.0)
-                
-                Spacer()
             }
         }
-        .ignoresSafeArea()
     }
 }
 
